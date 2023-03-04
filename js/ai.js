01c6d27fae0bfe88c395c3e5b73da0e92b6d597e
Khadiza083 <khadizakhatun4083@gmail.com>
2023-03-04 20:57:11 +0600

    features and integration added

--- js/ai.js
+++ js/ai.js
@@ -92,7 +92,61 @@ const loadDetails = async(id) =>{
 
 const displayLoadDetails = (data) =>{
     console.log(data);
-    console.log(data.input_output_examples[0].input)
+    // console.log(data.input_output_examples[0].input)
+
+    // modal left side
+    const modalLeftDiv = document.getElementById('modal-left-side')
+    const featureDetail = featuresDetails(data.features);
+    // console.log(featureDetail)
+    modalLeftDiv.innerHTML = `
+    <div class="card p-3 h-100 bg-warning-subtle mx-2">
+        <div class="">
+            <h6 class=""><b>${data.description}</b></h6>
+            <div id="pricing" class="d-flex gap-2 row" >
+                <div class="bg-light-subtle rounded-4 w-25 col">
+                    <div class="py-4 px-1" >
+                    <h6 class="text-center text-success">${data.pricing[0].price} ${data.pricing[0].plan}</h6>
+                    </div>
+                </div>
+                <div class="bg-light-subtle rounded-4 w-25 col">
+                    <div class="py-4 px-1">
+                    <h6 class="text-center text-warning">${data.pricing[1].price} ${data.pricing[1].plan}</h6>
+
+                    </div>
+                </div>
+                <div class="bg-light-subtle rounded-4 w-26 col">
+                    <div class="py-2 px-2">
+                    <h6 class="text-center text-danger" >${data.pricing[2].price} ${data.pricing[2].plan}</h6>
+                    </div>
+                </div>
+            <div class="row">
+                <div class="col" id="features-details">
+                    <h5 class="card-title">Features</h5>
+                    <p class="card-text">
+                    <ul>
+                    ${featureDetail.map(feature => `<li>${feature}</li>`).join("")}
+                </ul>
+                    </p>
+                </div>
+                <div class="col">
+                    <h5>Integrations</h5>
+                    <p class="card-text">
+                        <ul>
+                            ${data.integrations.map(integration => `<li>${integration}</li>`).join("")}
+                        </ul>
+
+                    </p>
+                </div>
+                   
+            </div>
+
+
+        </div>
+    </div>
+    `
+
+
+    // modal right side
     const modalRightDiv = document.getElementById('modal-right-side')
     modalRightDiv.innerHTML = `
     <div class="card  p-4">
@@ -104,6 +158,14 @@ const displayLoadDetails = (data) =>{
     </div>
   </div>
     `
+}
+const featuresDetails = (features) =>{
+    let featureArr = []
+    for(const key in features){
+      
+        featureArr[key] = features[key].feature_name;
+    }
+    return (featureArr);
 }
 loadDetails()
 loadAi(6);
